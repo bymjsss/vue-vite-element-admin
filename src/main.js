a87133d51f8b05d2ea2eb442e7d7847b6e98a16d
花裤衩 <yongming.bai@outlook.com>
2022-07-06 17:37:11 +0800

    feat: 环境配置

--- src/main.js
+++ src/main.js
@@ -5,8 +5,9 @@ import pinia from '@/store/index'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
 const app = createApp(App);
+
 document.title = import.meta.env.VITE_APP_TITLE
-console.log('%c[MESSAGE]%cline:8%cimport.meta.env', 'color:#fff;background:#ee6f57;padding:3px;border-radius:2px', 'color:#fff;background:#1f3c88;padding:3px;border-radius:2px', 'color:#fff;background:rgb(114, 83, 52);padding:3px;border-radius:2px', import.meta.env)
+// alert(`当前运行环境：${import.meta.env.VITE_APP_MODE}`)
 // 注册element icons
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
   app.component(key, component)
