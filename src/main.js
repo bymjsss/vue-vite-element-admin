1efe1c6013d629fa2cc548b2b0c5baaa4697afa0
花裤衩 <yongming.bai@outlook.com>
2022-07-07 11:08:32 +0800

    feat: + 可能用到的依赖

--- src/main.js
+++ src/main.js
@@ -3,7 +3,9 @@ import router from '@/router'
 import App from './App.vue'
 import pinia from '@/store/index'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
-
+import xgplayer from 'xgplayer'
+import Music from 'xgplayer-music'
+console.log('%c[MESSAGE]%cline:7%cMusic', 'color:#fff;background:#ee6f57;padding:3px;border-radius:2px', 'color:#fff;background:#1f3c88;padding:3px;border-radius:2px', 'color:#fff;background:rgb(248, 214, 110);padding:3px;border-radius:2px', Music, xgplayer)
 const app = createApp(App);
 
 document.title = import.meta.env.VITE_APP_TITLE
